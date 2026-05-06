e94aee32666a8b14061232247b6c95c4cf829052
Batle <malabananatle329@gmail.com>
2026-05-06 22:37:40 +0200

    Delete Html directory

--- Html/Interaktive.js
+++
@@ -1,8 +0,0 @@
- 
-        const hamburger = document.querySelector(".hamburger");
-        const nav1Links2 = document.querySelector(".nav1-links2");
-
-        hamburger.addEventListener("click", () => {
-            nav1Links2.classList.toggle("active");
-        });
-        
